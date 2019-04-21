3a4c6925b20608e1c061e9d426ace193fe2a6a7a
sean <smmy87@mail.umsl.edu>
2019-04-20 23:36:41 -0500

    db outside of aws enviro

--- inventory_mgmt/inventory_mgmt/settings.py
+++ inventory_mgmt/inventory_mgmt/settings.py
@@ -96,10 +96,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         #'NAME': 'os.path.join(BASE_DIR, 'db.sqlite3')',
-        'NAME': 'ebdb',
+        'NAME': 'ebdb1',
         'USER': 'devpostgres',
         'PASSWORD': 'Superbad2', #'Megabad2',
-        'HOST': 'aafc7ocl2j68hf.cvdsecoqidto.us-west-2.rds.amazonaws.com',
+        'HOST': 'mydb.cvdsecoqidto.us-west-2.rds.amazonaws.com',
         'PORT': '5432'
     }
 }
