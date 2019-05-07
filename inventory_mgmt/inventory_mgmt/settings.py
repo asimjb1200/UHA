b5ebc8972e6dcd62704ebb4b3bb466e9e9181e99
sean <smmy87@mail.umsl.edu>
2019-05-07 14:24:50 -0500

    colllect static, set static root, (needed to get aws working with django static)

--- inventory_mgmt/inventory_mgmt/settings.py
+++ inventory_mgmt/inventory_mgmt/settings.py
@@ -145,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 LOGIN_URL = '/'
 LOGIN_REDIRECT_URL = '/home/'
 LOGOUT_REDIRECT_URL = 'inventory:login'
