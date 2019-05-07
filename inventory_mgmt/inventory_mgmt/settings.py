19ab45dd95a665952fa09a600d5e0fb5ec6e52b5
sean <smmy87@mail.umsl.edu>
2019-05-07 14:08:42 -0500

    debug=true

--- inventory_mgmt/inventory_mgmt/settings.py
+++ inventory_mgmt/inventory_mgmt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pg5%z$e($sch(ct6d!9zq6-s8lfsq*$3k0iw83s)%6^=va5vkv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['invmgmt.ipqbehhmqi.us-west-2.elasticbeanstalk.com', 
     'localhost', 
