f491fac1947b9cfb98b617a5605157460337f082
sean <smmy87@mail.umsl.edu>
2019-05-14 13:14:47 -0500

    UI fixes food detail pages & others

--- inventory_mgmt/inventory_mgmt/settings.py
+++ inventory_mgmt/inventory_mgmt/settings.py
@@ -108,11 +108,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         #'NAME': 'os.path.join(BASE_DIR, 'db.sqlite3')',
-        'NAME': 'ebdb',
-        'USER': 'dev',
-        'PASSWORD': 'Superbad2', #'Megabad2',
-        'HOST': 'mydb1.cvdsecoqidto.us-west-2.rds.amazonaws.com',
-        'PORT': '5432'
+        'NAME': 'inventory',
+        'USER': 'postgres',
+        'PASSWORD': 'devpostgres', #'Megabad2',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
